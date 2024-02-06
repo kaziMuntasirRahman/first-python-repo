8827e362fce8fdc5a821399c3da2ab9d8e5ac3c1
kazimuntasirrahman <kazimuntasirrahman@gmail.com>
2024-02-06 12:01:20 +0600

    list input in pythong

---
+++ list-as-input.py
@@ -0,0 +1,21 @@
+n = input("Enter a string of number: ")
+
+list = n.split()
+sum = 0
+
+for num in list:
+  sum = sum + int(num)
+
+print(sum)
+
+
+
+# n = input("Enter a string of numbers: ")
+# list = n.split()
+
+# sum = 0
+# for num in list:
+#   sum = sum + int(num)
+
+
+# print(sum)
